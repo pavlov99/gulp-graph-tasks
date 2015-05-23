694b49897a3d114d38df1109032f66404268fea3
Kirill Pavlov <kirill.pavlov@phystech.edu>
2015-05-23 20:07:03 +0800

    add executable script

---
+++ bin/gulp-graph-tasks.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+var gulp = require('gulp'),
+  gulpfilePath = './gulpfile.js';
+
+require(gulpfilePath);
+
+for (var name in gulp.tasks) {
+  console.log(name + ' -> {' + gulp.tasks[name].dep.join(' ') + '}');
+}
